d7778be5df8ed7783da2ec46cd55ed2191d54666
SammySalamiMan <53416599+ngogiahuy4cantjoin@users.noreply.github.com>
2026-04-15 21:29:50 +0700

    ignore. stuff for slide pic

--- MainSrc.cpp
+++ MainSrc.cpp
@@ -1,11 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include <string>
-using namespace std;
+        while (true) {
+            if (p.hands[0].hand.size() == 5) {
+                if (p.hands[0].getHandType() == NGULINH) stat.upPNLCount();
+                break;
+            }
+            int action = playerWantsXD(p.hands[0], d);
+            if (action == 1) {
+                match.dealCardToHand(p.hands[0]);
+                continue;
+            }
+            if (action == 0) {
+                break;
+            }
+        }
 
-int main() {
-
-    
-    return 0;
+        if (d.getHandType() == XIDACH || d.getHandType() == XIBANG) {
+    for (auto& p : match.players) {
+        int result = compareHands(p.hands[0], d);
+        doResultXD(p.hands[0], d, result, stat);
+    }
+    return;
 }

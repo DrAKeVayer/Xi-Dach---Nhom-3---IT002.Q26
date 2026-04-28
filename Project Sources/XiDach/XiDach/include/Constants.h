4191e18530bc66e0119edaa1475f38f3110d3845
Doan Anh Khuong <anhkhuongdoan754@gmail.com>
2026-04-28 13:51:31 +0700

    v0.14.1: Minor fixes

---
+++ Project Sources/XiDach/XiDach/include/Constants.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <windows.h> //for printing unicode symbols
+#include <functional>
+
+using namespace std;
+
+extern int rankTable[53];
+extern string rankName[53];
+
+void initRankTable(int rankTable[]);
+
+string getSuit(int x);
+
+void initRankName(string rankName[]);
+
+string getRankName(int x);
+
+enum HandType {
+    THUONG = 0,
+    NGULINH = 1,
+    XIDACH = 2,
+    XIBANG = 3
+};
+
+enum Action {
+    HIT = 1,
+    XET = 1,
+    XETALL = 0,
+    STAND = 0,
+    XET3 = 2,
+    XET4 = 3,
+    SPLIT = 4,
+    DDOUBLE = 5,
+    SURRENDER = 6
+};
+
+enum TIME {
+    INSURE = 0,
+    ALL = 1,
+    SPLITDOUBLE = 3,
+    SECONDSPLIT = 4,
+    FIRSTDOUBLE = 5,
+    HITSTAND = 6,
+    SECONDDOUBLE = 7
+};
+
+string getFileName(const string& base, const string& csv);
